9a20a4bd1639168e8db3907dc2db61b10d287887
Evgeniya Bayda <baje75128@gmail.com>
2021-07-07 12:58:54 +0300

    uppdate

--- js/main.js
+++ js/main.js
@@ -1,74 +1,14 @@
-const galleryItems = [
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
-  },
-];
-const galleryEl = document.querySelector('.js-gallery');
-const itemGalleryEl = document.querySelector('.gallery__item');
-const divLightboxEl = document.querySelector('.js-lightbox');
-const divLightboxOverlay = document.querySelector('.lightbox__overlay');
-const divLightboxContent = document.querySelector('.lightbox__content');
-const lightboxImage = document.querySelector('.lightbox__image');
+import galleryItems from './script.js';
+const refs = {
+  galleryEl: document.querySelector('.js-gallery'),
+  itemGalleryEl: document.querySelector('.gallery__item'),
+  divLightboxEl: document.querySelector('.js-lightbox'),
+  divLightboxOverlay: document.querySelector('.lightbox__overlay'),
+  divLightboxContent: document.querySelector('.lightbox__content'),
+  lightboxImage: document.querySelector('.lightbox__image'),
+  btnModalClose: document.querySelector('[data-action="close-lightbox]'),
+};
+
 const createGallery = ({ preview, original, description }) =>
   `<li class = gallery__item data-action="open-lightbox"><a class = gallery__link href = '${original}'><img class = gallery__image src = '${preview}'data-source = '${original}'
    alt = '${description}'/></a></li>`;
@@ -76,4 +16,21 @@ const showGallery = galleryItems.reduce(
   (acc, item) => acc + createGallery(item),
   '',
 );
-galleryEl.insertAdjacentHTML('beforeend', showGallery);
+refs.galleryEl.insertAdjacentHTML('beforeend', showGallery);
+refs.galleryEl.addEventListener('click', onOpenModal);
+// refs.btnModalClose.addEventListener('click', onCloseModal);
+function onOpenModal(event) {
+  event.preventDefault();
+  console.log(event.currentTarget);
+  refs.divLightboxEl.classList.add('is-open');
+  refs.divLightboxEl.classList.add('lightbox__content');
+
+  // const target = event.target;
+  // console.log(target);
+  // if (target.nodeName !== 'LI') {
+  //   return;
+  // }
+}
+// function onCloseModal() {
+//   refs.divLightboxEl.classList.remove('is-open');
+// }
